16e681e615e46e566961d941056b6ae692c955ac
AlienX <mark.phillips@axsemail.co.uk>
2013-02-14 00:51:48 +0000

    Starting the Tmux Pane Log

--- pages/tmuxpanelog.php
+++ pages/tmuxpanelog.php
@@ -2,7 +2,23 @@
 			<!-- breadcrumbs end -->
 			<div class="breadcrumb">
 				<h2>TMUX Pane Log</h2><hr style="color:#000000; background-color:#000000; height:3px;"><br />
-				TMUX Pane Log is a future feature, hold in there - it will be released soon!<br /><br />
-				-- AlienX
+				<table border="0" width="100%" cellpadding="0" cellspacing="0">
+					<tr>
+						<td width="5%">
+							<div align="center"><strong>Log ID</strong></div>
+						</td>
+						<td width="50%">
+							<div align="center"><strong>Pane Name</strong></div>
+						</td>
+						<td width="30%">
+							<div align="center"><strong>State</strong></div>
+						</td>
+						<td>
+							<div align="center"><strong>Time Date</strong></div>
+						</td>
+					</tr>
+				</table>
+						
+				<div id="list" style="overflow:scroll; overflow-x:hidden; height:100px;">
 				<br />
 			</div>
