17e41ad89866e95c74845efd7bfb0153d365decf
AlienX <mark.phillips@axsemail.co.uk>
2013-02-25 17:24:40 +0000

    Fixed the Dashboard link going to root dir.

--- includes/leftmenu.php
+++ includes/leftmenu.php
@@ -3,7 +3,7 @@
 	<div class="well nav-collapse sidebar-nav">
 		<ul class="nav nav-tabs nav-stacked main-menu">
 			<li class="nav-header hidden-tablet">Main</li>
-			<li><a class="ajax-link" href="/"><i class="icon-home"></i><span class="hidden-tablet"> Dashboard</span></a></li>
+			<li><a class="ajax-link" href="./"><i class="icon-home"></i><span class="hidden-tablet"> Dashboard</span></a></li>
 			<li class="nav-header hidden-tablet">Newznab Info</li>
 			<li><a class="ajax-link" href="recent"><i class="icon-list-alt"></i><span class="hidden-tablet"> Recently Added</span></a></li>
 			<li><a class="ajax-link" href="statistics"><i class="icon-eye-open"></i><span class="hidden-tablet"> Statistics</span></a></li>
