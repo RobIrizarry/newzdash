da2433d817e154a009a455bfad6f9b9023c375f2
Jan Grewe <jan@faked.org>
2013-02-26 12:33:13 +0100

    Check if Path is set and is a directory
    cleaned up, a lot

--- newzdash-upgrade.sh
+++ newzdash-upgrade.sh
@@ -1,33 +1,31 @@
 #!/bin/sh
 # Upgrade script for Newzdash.
 
-$NEWZDASH_PATH = "/var/www/newznab/www/newzdash"
+NEWZDASH_PATH = ""
 
-if [ -f "$NEWZDASH_PATH" ]
-then
+if [ -z "${NEWZDASH_PATH}" ] || [ ! -d "${NEWZDASH_PATH}" ]; then
   echo "Unable to locate $NEWZDASH_PATH, aborting"
+  exit 0
 fi
 
-# First check if there isn't a update going on (just to be safe)
-# Here for, we use a tmp file.
-if [ -f /tmp/.newzdash-upgrade ]
-then
+# check if there isn't a update going on (just to be safe)
+if [ -f "${NEWZDASH_PATH}/.upgrading" ]; then
+
   echo "Update directory already exists, are you running this twice?"
-  echo "If not, then please rm -f /tmp/.newzdash-update"
+  echo "If not, then please rm -f ${NEWZDASH_PATH}/.upgrading"
   exit
+
+else
+
+  # There isn't running any upgrade, so create tmp file.
+  touch "${NEWZDASH_PATH}/.upgrading"
+
+  echo "Requesting git repo stash and pull..."
+  cd "${NEWZDASH_PATH}"
+  git stash
+  git pull
+
+  # Remove the temp file since the update is completed.
+  rm "${NEWZDASH_PATH}/.upgrading"
+
 fi
-
-# There isn't running any upgrade, so create tmp file.
-touch /tmp/.newzdash-upgrade
-wait
-
-echo "Requesting git repo stash and pull..."
-cd $NEWZDASH_PATH
-git stash
-git pull
-
-echo "Waiting for git to update the repo..."
-wait
-
-# Remove the temp file since the update is completed.
-rm /tmp/.newzdash-upgrade
