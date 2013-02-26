1cc9cf192b998f2e9a50789b78335d4a9a8c7ceb
AlienXAXS <mark.phillips@axsemail.co.uk>
2013-02-26 11:21:02 +0000

    Update newzdash-upgrade.sh

--- newzdash-upgrade.sh
+++ newzdash-upgrade.sh
@@ -1,20 +1,33 @@
 #!/bin/sh
 # Upgrade script for Newzdash.
-#
+
+$NEWZDASH_PATH = "/var/www/newznab/www/newzdash"
+
+if [ -f "$NEWZDASH_PATH" ]
+then
+  echo "Unable to locate $NEWZDASH_PATH, aborting"
+fi
+
 # First check if there isn't a update going on (just to be safe)
 # Here for, we use a tmp file.
 if [ -f /tmp/.newzdash-upgrade ]
 then
-echo "There is already an update going on, so exit."
-exit
+  echo "Update directory already exists, are you running this twice?"
+  echo "If not, then please rm -f /tmp/.newzdash-update"
+  exit
 fi
+
 # There isn't running any upgrade, so create tmp file.
 touch /tmp/.newzdash-upgrade
 wait
-echo "Do the git pull (first cd in to the right path, edit to match yours!)."
-cd /var/www/newznab/www/newzdash
+
+echo "Requesting git repo stash and pull..."
+cd $NEWZDASH_PATH
+git stash
 git pull
-echo "Wait until the pull is complete"
+
+echo "Waiting for git to update the repo..."
 wait
+
 # Remove the temp file since the update is completed.
 rm /tmp/.newzdash-upgrade
