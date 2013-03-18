1ec949de8283e405d6cba0b4e1e57cfe8b07c993
AlienXAXS <mark.phillips@axsemail.co.uk>
2013-03-18 14:53:58 +0000

    Added in a non-seo enabled links part to nginx
    user issues

--- includes/leftmenu.php
+++ includes/leftmenu.php
@@ -2,6 +2,22 @@
 <div class="span2 main-menu-span">
 	<div class="well nav-collapse sidebar-nav">
 		<ul class="nav nav-tabs nav-stacked main-menu">
+		
+			<!-- Uncomment this block to enable non-seo links -->
+			
+			<!-- Non SEO Enabled Links -->
+			<!--
+			<li class="nav-header hidden-tablet">Main</li>
+			<li><a class="ajax-link" href="index.php?page=dashboard"><i class="icon-home"></i><span class="hidden-tablet"> Dashboard</span></a></li>
+			<li class="nav-header hidden-tablet">Newznab Info</li>
+			<li><a class="ajax-link" href="index.php?page=recent"><i class="icon-list-alt"></i><span class="hidden-tablet"> Recently Added</span></a></li>
+			<li><a class="ajax-link" href="index.php?page=statistics"><i class="icon-eye-open"></i><span class="hidden-tablet"> Statistics</span></a></li>
+			<li class="nav-header hidden-tablet">TMUX Info</li>
+			<li><a class="ajax-link" href="index.php?page=tmuxpanelog"><i class="icon-list-alt"></i><span class="hidden-tablet"> TMUX Pane Log</span></a></li>
+			-->
+		
+		
+			<!-- SEO Enabled Links -->
 			<li class="nav-header hidden-tablet">Main</li>
 			<li><a class="ajax-link" href="dashboard"><i class="icon-home"></i><span class="hidden-tablet"> Dashboard</span></a></li>
 			<li class="nav-header hidden-tablet">Newznab Info</li>
